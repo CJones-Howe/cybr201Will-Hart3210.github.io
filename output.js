6c48d0cd822de173b100a5a631f6162008b29e47
cybr201Will-Hart3210.github.io <whart@albany.edu>
2025-03-24 19:31:57 -0400

    Create output.js
    Java script output

---
+++ output.js
@@ -0,0 +1,12 @@
+function showInnerHTML() {
+	document.getElementById("output1").innerHTML = "Hello from Willy! I love watching basketball and walking my dog.";
+}
+function useDocumentWrite() {
+	document.write("I am very excited to finally finish school and graduate!");
+}
+function showAlert() {
+	alert("My favorite hobbies are playing basketball and video games.");
+}
+function showConsole() {
+	console.log("One fun fact about me is I have 5 brothers.");
+} 
