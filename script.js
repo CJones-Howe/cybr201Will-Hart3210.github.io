1ad5983eac9d3ef5877bd211bdff867fcfeb9dd9
cybr201Will-Hart3210.github.io <whart@albany.edu>
2025-05-06 18:44:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("funBtn");
   const msg = document.getElementById("msg");
+
   const quotes = [
     "Evil is evil. Lesser, greater, middling... it's all the same.",
     "People like to invent monsters and monstrosities.",
     "I'm not a good man. I'm a witcher.",
     "Hmmm... wind's howling.",
-    "When you know about something it stops being a nightmare."
+    "When you know about something it stops being a nightmare. When you know how it works, it stops being magic."
   ];
+
   if (btn) {
     btn.addEventListener("click", () => {
       const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
