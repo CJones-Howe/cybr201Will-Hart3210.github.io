a5fa4510a547090d704993bf1ee2ac3e84b0bf2b
cybr201Will-Hart3210.github.io <whart@albany.edu>
2025-05-06 18:15:33 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Output in different ways
-document.getElementById("demo").innerHTML = "This is written in JavaScript!";
-alert("This is an alert box!);
-console.log("This is logged in the console."); 
